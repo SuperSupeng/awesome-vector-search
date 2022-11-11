c9b5c5221a833f87538c8ec7942572d988b91dea
super <supeng842499467@gmail.com>
2022-11-11 14:28:16 +0800

    init

---
+++ update.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import urllib3
+from lxml import etree
+import html
+import re
+
+baseUrl = 'https://api.github.com/repos/OWNER/REPO/commits'
+
+headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'} 
+
+f = open('README.md', 'w+')
+f.write('# awesome-vector-search\n')
+f.close 
