01a9efb75cbb548e5f5faebd68d955d313f0feff
super <supeng842499467@gmail.com>
2022-11-11 14:33:05 +0800

    init

--- update.py
+++ update.py
@@ -5,6 +5,7 @@ import html
 import re
 
 baseUrl = 'https://api.github.com/repos/OWNER/REPO/commits'
+# https://docs.github.com/cn/rest/commits/commits
 
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'} 
 
